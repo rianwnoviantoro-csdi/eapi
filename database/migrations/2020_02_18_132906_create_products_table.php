66efc74159e86c77e249b84bc6d954c0f8902973
Mongoding <tenkeylost@gmail.com>
2020-02-18 20:56:29 +0700

    migration created for product and reviews

--- database/migrations/2020_02_18_132906_create_products_table.php
+++ database/migrations/2020_02_18_132906_create_products_table.php
@@ -6,26 +6,31 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreateProductsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('products', function (Blueprint $table) {
-            $table->increments('id');
-            $table->timestamps();
-        });
-    }
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::create('products', function (Blueprint $table) {
+      $table->increments('id');
+      $table->string('name');
+      $table->text('detail');
+      $table->integer('price');
+      $table->integer('stock');
+      $table->integer('discount');
+      $table->timestamps();
+    });
+  }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('products');
-    }
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::dropIfExists('products');
+  }
 }
--- database/migrations/2020_02_18_132926_create_reviews_table.php
+++ database/migrations/2020_02_18_132926_create_reviews_table.php
@@ -6,26 +6,31 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreateReviewsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('reviews', function (Blueprint $table) {
-            $table->increments('id');
-            $table->timestamps();
-        });
-    }
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::create('reviews', function (Blueprint $table) {
+      $table->increments('id');
+      $table->integer('product_id')->unsigned()->index();
+      $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+      $table->string('customer');
+      $table->text('review');
+      $table->integer('star');
+      $table->timestamps();
+    });
+  }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('reviews');
-    }
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::dropIfExists('reviews');
+  }
 }
