ae2b602edc26076c43a6afe0e4734cf16e12311a
Mongoding <tenkeylost@gmail.com>
2020-02-19 00:01:07 +0700

    create product

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,13 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ProductRequest;
 use App\Http\Resources\Product\ProductCollection;
 use App\Http\Resources\Product\ProductResource;
 use App\Model\Product;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class ProductController extends Controller
 {
+  public function __construct() {
+    $this->middleware('auth:api')->except('index','show');
+  }
+
   public function index() {
     return ProductCollection::collection(Product::paginate(20));
   }
@@ -17,8 +23,17 @@ class ProductController extends Controller
     //
   }
   
-  public function store(Request $request) {
-    //
+  public function store(ProductRequest $request) {
+    $product = new Product;
+    $product->name      = $request->name;
+    $product->detail    = $request->description;
+    $product->stock     = $request->stock;
+    $product->price     = $request->price;
+    $product->discount  = $request->discount;
+    $product->save();
+    return response([
+      'data' => new ProductResource($product)
+    ], Response::HTTP_CREATED);
   }
   
   public function show(Product $product) {
