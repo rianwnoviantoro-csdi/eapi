cfa17b5009322e1681d8e908cc48cc592266cb24
Mongoding <tenkeylost@gmail.com>
2020-02-18 22:10:43 +0700

    modified product detail

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -11,8 +11,13 @@ class ProductResource extends Resource
       'name'        => $this->name,
       'description' => $this->detail,
       'price'       => $this->price,
-      'stock'       => $this->stock,
-      'discount'    => $this->discount
+      'stock'       => $this->stock == 0 ? 'Out of Stock' : $this->stock,
+      'discount'    => $this->discount,
+      'finalPrice'  => round((1-($this->discount/100)) * $this->price, 2),
+      'rating'      => $this->reviews->count() > 0 ? round($this->reviews->sum('star')/$this->reviews->count()) : 'No rating yet',
+      'href'        => [
+        'reviews' => route('reviews.index', $this->id) 
+      ]
     ];
   }
 }
