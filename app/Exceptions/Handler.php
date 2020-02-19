10983a329801848a8e302c5cf2e29c3d035229ef
Mongoding <tenkeylost@gmail.com>
2020-02-19 19:47:33 +0700

    handle exception

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,51 +3,39 @@
 namespace App\Exceptions;
 
 use Exception;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
-class Handler extends ExceptionHandler
-{
-    /**
-     * A list of the exception types that are not reported.
-     *
-     * @var array
-     */
-    protected $dontReport = [
-        //
-    ];
-
-    /**
-     * A list of the inputs that are never flashed for validation exceptions.
-     *
-     * @var array
-     */
-    protected $dontFlash = [
-        'password',
-        'password_confirmation',
-    ];
-
-    /**
-     * Report or log an exception.
-     *
-     * This is a great spot to send exceptions to Sentry, Bugsnag, etc.
-     *
-     * @param  \Exception  $exception
-     * @return void
-     */
-    public function report(Exception $exception)
-    {
-        parent::report($exception);
+class Handler extends ExceptionHandler {
+  protected $dontReport = [
+    
+  ];
+  
+  protected $dontFlash = [
+    'password',
+    'password_confirmation',
+  ];
+  
+  public function report(Exception $exception) {
+    parent::report($exception);
+  }
+  
+  public function render($request, Exception $exception) {
+    if ($request->expectsJson()) {
+      if ($exception instanceof ModelNotFoundException) {
+        return response()->json([
+          'error' => 'Product model not found',
+        ], Response::HTTP_NOT_FOUND);
+      }
+      if ($exception instanceof NotFoundHttpException) {
+        return response()->json([
+          'error' => 'Incorect route',
+        ], Response::HTTP_NOT_FOUND);
+      }
     }
-
-    /**
-     * Render an exception into an HTTP response.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Exception  $exception
-     * @return \Illuminate\Http\Response
-     */
-    public function render($request, Exception $exception)
-    {
-        return parent::render($request, $exception);
-    }
+    // dd($exception);
+    return parent::render($request, $exception);
+  }
 }
